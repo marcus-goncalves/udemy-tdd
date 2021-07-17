56df10fdd56ea3590ae9d0f8654af4de000e4fce
Marcus Gonçalves <nvrrox@gmail.com>
2021-07-17 16:40:00 -0300

    RED - 2nd use case

--- section6/test_line_reader.py
+++ section6/test_line_reader.py
@@ -4,8 +4,22 @@ USE CASES
 - read_from_file returns correct string
 - read_from_files throws exception when file doesn't exist
 """
+from typing import Match
 from .line_reader import read_from_file
+from unittest.mock import MagicMock
 
 
 def test_CallReadFromFile() -> None:
     read_from_file("blac")
+
+
+def test_ReturnsCorrectString(monkeypatch) -> None:
+    mock_file = MagicMock()
+    mock_file.read_line = MagicMock(return_value='test line')
+
+    mock_open = MagicMock(return_value=mock_file)
+    monkeypatch.setattr("builtins.open", mock_open)
+    result = read_from_file("blah")
+
+    mock_open.assert_called_once_with("blah", "r")
+    assert result == "test line"
