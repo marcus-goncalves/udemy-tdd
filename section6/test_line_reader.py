b12512f03d51022054d8ace739357d4cb4a73810
Marcus Gonçalves <nvrrox@gmail.com>
2021-07-17 16:50:41 -0300

    RED - 3rd use case

--- section6/test_line_reader.py
+++ section6/test_line_reader.py
@@ -6,6 +6,7 @@ USE CASES
 """
 from .line_reader import read_from_file
 from unittest.mock import MagicMock
+import pytest
 
 
 def test_ReturnsCorrectString(monkeypatch) -> None:
@@ -18,3 +19,16 @@ def test_ReturnsCorrectString(monkeypatch) -> None:
 
     mock_open.assert_called_once_with("blah", "r")
     assert result == "test line"
+
+
+def test_ExceptionDoesntExists(monkeypatch) -> None:
+    mock_file = MagicMock()
+    mock_file.readline = MagicMock(return_value='test line')
+
+    mock_open = MagicMock(return_value=mock_file)
+    monkeypatch.setattr("builtins.open", mock_open)
+
+    mock_exists = MagicMock(return_value=False)
+    monkeypatch.setattr('os.path.exists', mock_exists)
+    with pytest.raises(Exception):
+        result = read_from_file('blah')
