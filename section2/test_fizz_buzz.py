1dbe632fb217d8535158079222b9b3b90cdda219
Marcus Gonçalves <nvrrox@gmail.com>
2021-07-17 12:51:39 -0300

    RED - return string of 1 when 1 is passed

--- section2/test_fizz_buzz.py
+++ section2/test_fizz_buzz.py
@@ -1,13 +1,13 @@
 """
 USE CASES:
 - Call FizzBuzz function
-- Get 1 when 1 is passed
-- Get 2 when 2 is passed
-- Get Fizz when 3 is passed
-- Get Buzz when 5 is passed
-- Get Fizz when a multiple of 3 is passed (6)
-- Get Buzz when a multiple of 5 is passed (10)
-- Gut FizzBuzz when a multiple of 3 and 5 is passed (15)
+- Get "1" when 1 is passed
+- Get "2" when 2 is passed
+- Get "Fizz" when 3 is passed
+- Get "Buzz" when 5 is passed
+- Get "Fizz" when a multiple of 3 is passed (6)
+- Get "Buzz" when a multiple of 5 is passed (10)
+- Gut "FizzBuzz" when a multiple of 3 and 5 is passed (15)
 """
 
 
@@ -17,3 +17,8 @@ def fizz_buzz(number: int):
 
 def test_canCallFizzBuzz():
     fizz_buzz(1)
+
+
+def test_return1With1PassedIn():
+    result = fizz_buzz(1)
+    assert result == "1"
